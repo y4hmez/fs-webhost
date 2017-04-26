97ab3e1544f46cdf26ec35db7c4654e7a615783a
James Oakley <JOakley@compasshealth.local>
2017-04-27 09:28:51 +1200

    Azure Persistant with error and logging compiles...

--- fs-webhost/Global.asax.fs
+++ fs-webhost/Global.asax.fs
@@ -21,6 +21,27 @@ type Agent<'T> = MailboxProcessor<'T>
 
 type HttpRouteDefaults = { Controller : string; Id : obj }  
 
+type ErrorsInAzureBlobs(blobContainer : CloudBlobContainer) =
+    let getId (d : DateTime) =
+        String.Join(
+            "/",
+            [
+                d.Year.ToString()
+                d.Month.ToString()
+                d.Day.ToString()
+                Guid.NewGuid().ToString()
+            ]) |> sprintf "%s.txt"
+
+    member this.Write e = 
+        let id = getId DateTimeOffset.UtcNow.Date
+        let b = blobContainer.GetBlockBlobReference id
+        b.Properties.ContentType <- "text/plain; charset=utf-8"
+        b.UploadText(e.ToString())
+
+    interface Filters.IExceptionFilter with
+        member this.AllowMultiple = true
+        member this.ExecuteExceptionFilterAsync(actionExecutedContext, cancellationToken) = 
+            System.Threading.Tasks.Task.Run(fun() -> this.Write  actionExecutedContext.Exception)            
 
 module AzureQ = 
     let enqueue (q: Queue.CloudQueue) msg = 
@@ -36,11 +57,14 @@ type Global() =
     inherit Web.HttpApplication()
     member this.Application_Start (sender :obj) (e : EventArgs) = 
             let seatingCapacity = 10
-                        
-            //let reservations = ReservationsInFiles(DirectoryInfo(Web.HttpContext.Current.Server.MapPath("~/ReservationStore")))            
-            //let notifications = NotificationsInFiles(DirectoryInfo(Web.HttpContext.Current.Server.MapPath("~/NotificationStore")))
-
+                                    
             let storageAccount = CloudConfigurationManager.GetSetting "storageConnectionString" |> CloudStorageAccount.Parse
+
+            let errorContainer = storageAccount.CreateCloudBlobClient().GetContainerReference("errors");
+            errorContainer.CreateIfNotExists() |> ignore
+            let errorHandler = ErrorsInAzureBlobs(errorContainer)
+
+            GlobalConfiguration.Configuration.Filters.Add errorHandler
 
             //reservation queue
             let rq = storageAccount.CreateCloudQueueClient().GetQueueReference("reservations")
@@ -71,34 +95,37 @@ type Global() =
             |> ignore
                                        
             let handleR (msg : Queue.CloudQueueMessage) = 
-                let json = msg.AsString
-                let cmd = 
-                    JsonConvert.DeserializeObject<Envelope<ReservationCmd>> json
-                let condition = reservations.GetAccessCondition cmd.Item.Date
-                let newReservations = Handle seatingCapacity reservations cmd
-                match newReservations with
-                        | Some(r) -> 
-                            reservationSubject.OnNext(r, cmd.Id, condition)
-                            notificationSubject.OnNext 
-                                {
-                                    About = cmd.Id
-                                    Type = "Success"
-                                    Message = sprintf "completed %s " (cmd.Item.Date.ToString "yyyy.MM.dd")
-                                }
-                        | _ -> 
-                            notificationSubject.OnNext 
+                try
+                    let json = msg.AsString
+                    let cmd = JsonConvert.DeserializeObject<Envelope<ReservationCmd>> json
+                    let condition = reservations.GetAccessCondition cmd.Item.Date
+                    let newReservations = Handle seatingCapacity reservations cmd
+                    match newReservations with
+                            | Some(r) -> 
+                                reservationSubject.OnNext(r, cmd.Id, condition)
+                                notificationSubject.OnNext 
                                     {
                                         About = cmd.Id
-                                        Type = "Failure"
-                                        Message = sprintf "didnt work %s " (cmd.Item.Date.ToString "yyyy.MM.dd")
+                                        Type = "Success"
+                                        Message = sprintf "completed %s " (cmd.Item.Date.ToString "yyyy.MM.dd")
                                     }
-                rq.DeleteMessage msg
+                            | _ -> 
+                                notificationSubject.OnNext 
+                                        {
+                                            About = cmd.Id
+                                            Type = "Failure"
+                                            Message = sprintf "didnt work %s " (cmd.Item.Date.ToString "yyyy.MM.dd")
+                                        }
+                    rq.DeleteMessage msg
+                with e -> errorHandler.Write e
 
             let handleN (msg : Queue.CloudQueueMessage) = 
-                            let json = msg.AsString
-                            let notification = JsonConvert.DeserializeObject<Envelope<NotificationEvt>> json
-                            notifications.Write notification                
-                            nq.DeleteMessage msg
+                try
+                    let json = msg.AsString
+                    let notification = JsonConvert.DeserializeObject<Envelope<NotificationEvt>> json
+                    notifications.Write notification                
+                    nq.DeleteMessage msg
+                with e -> errorHandler.Write e
            
             System.Reactive.Linq.Observable.Interval(TimeSpan.FromSeconds 10.0)
             |> Observable.map (fun _ -> AzureQ.dequeue rq)
